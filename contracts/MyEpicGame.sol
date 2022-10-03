e11eb66965714c7aeb61cbfea17bea5ddfd706d9
unknown <md.ridgeway.work@gmail.com>
2022-10-03 18:08:52 -0500

    Add character attributes

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,48 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT. NICE.");
+    // Character's attributes held in a struct.
+    // (add defense, crit chance, etc if needed)
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    // Array to help us hold the default data for characters
+    CharacterAttributes[] defaultCharacters;
+
+    // Data passed in to the contract when it's first created initializing the characters.
+    // We're going to actually pass these values in from run.js.
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        // Loop through all the characters, and save the values in the contract
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
     }
 }
